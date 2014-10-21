25ec578f4f64d58f4d0c8f4f544823262d4c05be
Bin <boblan900123@gmail.com>
2014-10-20 17:39:19 -0700

    add exchange Information

---
+++ src/it/java/edu/cmu/sv/ws/ssnoc/test/AnalyzeSocialNetworkTest.java
@@ -0,0 +1,24 @@
+package edu.cmu.sv.ws.ssnoc.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AnalyzeSocialNetworkTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
